8b139e3f6ea68a1b671fc17b1d7e3ebca7ef0577
mypersonalgithubaccount <noisebroadcast@gmail.com>
2018-04-13 22:52:47 -0700

    change base and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,13 @@ import { Button } from "react-bootstrap";
 
 class Footer extends React.Component {
   render() {
-    const logOutButton =
-      <Button bsSize="large" onClick={this.props.logOut}> Log Out </Button>
-    const signInModal =
-      <SignInModal authenticate={this.props.authenticate} />
+    const logOutButton = (
+      <Button bsSize="large" onClick={this.props.logOut}>
+        {" "}
+        Log Out{" "}
+      </Button>
+    );
+    const signInModal = <SignInModal authenticate={this.props.authenticate} />;
     return (
       <React.Fragment>
         <div className="footer">
@@ -18,6 +21,12 @@ class Footer extends React.Component {
             <b>
               <a className="myname" href="https://nvincenthill.github.io/">
                 Nicholas Vincent-Hill &nbsp;
+              </a>
+            </b>
+            and &nbsp;
+            <b>
+              <a className="myname" href="https://avhdesign.win/">
+                Alexander Vincent-Hill &nbsp;
               </a>
             </b>
             in &nbsp;
