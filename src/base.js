265682d6ef67cf761719c22acbb87e2ac2c497a5
mypersonalgithubaccount <noisebroadcast@gmail.com>
2018-04-13 22:52:47 -0700

    change base and footer

---
+++ src/base.js
@@ -0,0 +1,16 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// This is a named export
+export { firebaseApp };
+
+// this is a default export
+export default base;
