7ea93a9ba4430eb29af3fe77617fed013e74de7c
mypersonalgithubaccount <noisebroadcast@gmail.com>
2018-04-15 18:40:40 -0700

    fix base and auth

--- src/base.js
+++ src/base.js
@@ -1,13 +1,15 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
+//remove
+const databaseURL = "https://mealplanner-196022.firebaseio.com"
+
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL
+  databaseURL: databaseURL
 });
 
-console.log(process.env);
 const base = Rebase.createClass(firebaseApp.database());
 
 // This is a named export
