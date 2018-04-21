bcaf628c181f0cfcb430ba65fde14ed88e726895
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-04-20 20:40:47 -0700

    make mobile styling responsive

--- src/components/MealPanda.js
+++ src/components/MealPanda.js
@@ -23,7 +23,7 @@ class MealPanda extends React.Component {
     let pandaTop = <img id="logo" src="/images/mealpanda.png" alt="logo" />;
 
     return (
-      <Collapse isOpened={!this.props.generateButtonHidden}>
+      <Collapse isOpened={!this.props.pandaHidden} springConfig={{stiffness: 180, damping: 12}}>
       <ReactCSSTransitionGroup
         transitionName="logo-animation"
         transitionAppear={true}
