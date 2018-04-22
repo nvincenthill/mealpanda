ce453677d146149e9f6361fc57e8e2edf8059c43
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-04-21 17:23:56 -0700

    fix hover glitch

--- src/components/MealPanda.js
+++ src/components/MealPanda.js
@@ -20,7 +20,7 @@ class MealPanda extends React.Component {
   }
 
   render() {
-    let pandaTop = <img id="logo" src="/images/mealpanda.png" alt="logo" />;
+    let pandaTop = <img id="logo" src="/images/mealpanda.png" alt="logo"/>;
 
     return (
       <Collapse isOpened={!this.props.pandaHidden} springConfig={{stiffness: 180, damping: 12}}>
