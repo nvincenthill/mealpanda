c7578981c6da61a9838dc0bdd3d56d163fe39ee9
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-06-22 22:32:16 -0700

    Hide API keys

---
+++ src/base.js
@@ -0,0 +1,20 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+import config from "./components/config";
+
+//remove
+const databaseURL = "https://mealplanner-196022.firebaseio.com";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: config.APIKey,
+  authDomain: "mealplanner-196022.firebaseapp.com",
+  databaseURL: databaseURL
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// This is a named export
+export { firebaseApp };
+
+// this is a default export
+export default base;
